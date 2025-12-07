8520c3c73a479aad9a474529050c775f3bd62d1c
Martin Slabý <martin.slaby@omnetic.com>
2025-12-07 11:01:44 +0100

    chore: update JSDOC

--- days/02/part1.ts
+++ days/02/part1.ts
@@ -1,3 +1,16 @@
+/**
+ * Advent of Code 2025
+ *
+ * @description 
+ * Solution for Advent of Code 2025, Day 2, Part 1.
+ * Problem details and requirements can be found at: https://adventofcode.com/2025/day/2
+ *
+ * @note
+ * This code was created entirely without any AI usage, outside help or any coding assistant. 
+ * The solution was developed independently through manual problem analysis, algorithmic thinking,
+ * and personal coding skills.
+ */
+
 import { input } from "./input";
 
 let result = 0;
--- days/03/part1.ts
+++ days/03/part1.ts
@@ -1,3 +1,16 @@
+/**
+ * Advent of Code 2025
+ *
+ * @description 
+ * Solution for Advent of Code 2025, Day 3, Part 1.
+ * Problem details and requirements can be found at: https://adventofcode.com/2025/day/3
+ *
+ * @note
+ * This code was created entirely without any AI usage, outside help or any coding assistant. 
+ * The solution was developed independently through manual problem analysis, algorithmic thinking,
+ * and personal coding skills.
+ */
+
 import { input } from "./input";
 
 const lines = input.split("\n");
